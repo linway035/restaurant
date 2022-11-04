078f70489a88130c224212b79291c0aaaad170aa
linway035 <wayneck0138@gmail.com>
2022-11-04 17:21:31 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,16 @@
 const express = require("express");
 const router = express.Router();
 
-// 引入 home 模組程式碼
+// 引入  模組程式碼
 const home = require("./modules/home");
-router.use("/", home);
-
-// 引入 restaurants 模組程式碼
 const restaurants = require("./modules/restaurants");
-router.use("/restaurants", restaurants);
-
-// 引入 search 模組程式碼
 const search = require("./modules/search");
+const users = require("./modules/users");
+
+router.use("/restaurants", restaurants);
 router.use("/search", search);
+router.use("/users", users);
+router.use("/", home);
 
 // 匯出路由器
 module.exports = router;
