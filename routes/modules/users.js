7b65d0ddcf7a5cb8aa0181b7e2b0a9e961240b28
linway035 <wayneck0138@gmail.com>
2022-11-04 20:30:32 +0800

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const User = require("../../models/user");
 // 引用 passport
 const passport = require("passport");
+const bcrypt = require("bcryptjs");
 
 router.get("/login", (req, res) => {
   res.render("login");
@@ -56,11 +57,16 @@ router.post("/register", (req, res) => {
         });
       } else {
         // 如果還沒註冊：寫入資料庫
-        return User.create({
-          name,
-          email,
-          password,
-        })
+        return bcrypt
+          .genSalt(10)
+          .then((salt) => bcrypt.hash(password, salt))
+          .then((hash) =>
+            User.create({
+              name,
+              email,
+              password: hash,
+            })
+          )
           .then(() => res.redirect("/"))
           .catch((err) => console.log(err));
       }
