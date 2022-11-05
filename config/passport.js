6d823080657cbb2b6ff5b887861c899d631cb197
linway035 <wayneck0138@gmail.com>
2022-11-05 17:07:44 +0800

    style: JavaScript Standard Style

--- config/passport.js
+++ config/passport.js
@@ -1,17 +1,17 @@
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const FacebookStrategy = require("passport-facebook").Strategy;
-const User = require("../models/user"); // 易忘
-const bcrypt = require("bcryptjs");
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
+const User = require('../models/user') // 易忘
+const bcrypt = require('bcryptjs')
 
 module.exports = (app) => {
   // 初始化 Passport 模組
-  app.use(passport.initialize());
-  app.use(passport.session());
+  app.use(passport.initialize())
+  app.use(passport.session())
   // 設定本地登入策略
   passport.use(
     new LocalStrategy(
-      { usernameField: "email", passReqToCallback: true },
+      { usernameField: 'email', passReqToCallback: true },
       (req, email, password, done) => {
         User.findOne({ email })
           .then((user) => {
@@ -21,24 +21,24 @@ module.exports = (app) => {
               return done(
                 null,
                 false,
-                req.flash("warning_msg", "That email is not registered!")
-              );
+                req.flash('warning_msg', 'That email is not registered!')
+              )
             }
             return bcrypt.compare(password, user.password).then((isMatch) => {
               if (!isMatch) {
                 return done(
                   null,
                   false,
-                  req.flash("warning_msg", "Email or Password incorrect.")
-                );
+                  req.flash('warning_msg', 'Email or Password incorrect.')
+                )
               }
-              return done(null, user);
-            });
+              return done(null, user)
+            })
           })
-          .catch((err) => done(err, false));
+          .catch((err) => done(err, false))
       }
     )
-  );
+  )
   // 設定Facebook登入策略
   passport.use(
     new FacebookStrategy(
@@ -46,14 +46,14 @@ module.exports = (app) => {
         clientID: process.env.FACEBOOK_ID,
         clientSecret: process.env.FACEBOOK_SECRET,
         callbackURL: process.env.FACEBOOK_CALLBACK,
-        profileFields: ["email", "displayName"],
+        profileFields: ['email', 'displayName']
       },
       (accessToken, refreshToken, profile, done) => {
         // console.log(profile);
-        const { name, email } = profile._json;
+        const { name, email } = profile._json
         User.findOne({ email }).then((user) => {
-          if (user) return done(null, user);
-          const randomPassword = Math.random().toString(36).slice(-8);
+          if (user) return done(null, user)
+          const randomPassword = Math.random().toString(36).slice(-8)
           bcrypt
             .genSalt(10)
             .then((salt) => bcrypt.hash(randomPassword, salt))
@@ -61,23 +61,23 @@ module.exports = (app) => {
               User.create({
                 name,
                 email,
-                password: hash,
+                password: hash
               })
             )
             .then((user) => done(null, user))
-            .catch((err) => done(err, false));
-        });
+            .catch((err) => done(err, false))
+        })
       }
     )
-  );
+  )
   // 設定序列化與反序列化
   passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
+    done(null, user.id)
+  })
   passport.deserializeUser((id, done) => {
     User.findById(id)
       .lean()
       .then((user) => done(null, user))
-      .catch((err) => done(err, null));
-  });
-};
+      .catch((err) => done(err, null))
+  })
+}
